9f0bdaf7267d9c58e5a854dc99c823d30e41dcc7
unknown <gabrielsouza.ccampos@gmail.com>
2021-10-05 22:27:14 -0300

    Finalizando🎉

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,83 @@
 const express = require("express");
+const Article = require("../models/articles");
 const route = express.Router();
 
 route.get("/new", (req, res) => {
-  res.render("articles/new");
+  res.render("articles/new", { article: new Article() });
 });
 
-route.post("/", (req, res) => {});
+route.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
+  if (article == null) res.redirect("/");
+  res.render("articles/show", { article: article });
+});
+
+route.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+route.post(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
+
+route.post("/", async (req, res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.slug}`);
+  } catch (err) {
+    console.log(err);
+    res.render("articles/new", { article: article });
+  }
+});
+
+route.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+route.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
+});
+
+route.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article });
+    }
+  };
+}
+
 module.exports = route;
--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const Article = require("./models/articles");
 const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+mongoose.connect("mongodb://localhost/blog");
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test article",
-      createdAt: new Date(),
-      description: "Teste descriptionm",
-    },
-    {
-      title: "Test article",
-      createdAt: new Date(),
-      description: "Teste descriptionm",
-    },
-  ];
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
 });
 
-app.listen(5000);
+app.use("/articles", articleRouter);
+
+app.listen(8000);
